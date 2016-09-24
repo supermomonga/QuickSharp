1d0795501896773d8646c558928b97a8e369ca81
supermomonga <hi@supermomonga.com>
2016-09-25 02:29:29 +0900

    remove unnecessary using statement

--- QuickSharp/Program.cs
+++ QuickSharp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.CommandLine;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 
