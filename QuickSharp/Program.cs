8afd81a160a1deb420b795650eb74d24f005388d
supermomonga <hi@supermomonga.com>
2017-07-20 11:22:54 +0900

    use Microsoft.Extensions.CommandLineUtils to parse command line option

--- QuickSharp/Program.cs
+++ QuickSharp/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.CommandLine;
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace QuickSharp
 {
@@ -10,33 +11,49 @@ namespace QuickSharp
     {
         public static void Main(string[] args)
         {
-            MainAsync(args).GetAwaiter().GetResult();
-        }
-        public static async Task MainAsync(string[] args)
-        {
-            // Get file name
-            var file = "./Program.cs";
-            ArgumentSyntax.Parse(args, syntax =>
-            {
-                syntax.DefineOption("f|file", ref file, "File to execute");
-            });
+            var app = new CommandLineApplication();
+            app.Name = "QuickSharp";
+            app.HelpOption("-?|-h|--help");
+            app.Argument("file", "File to execure");
 
-            // Get file path
-            try
-            {
-                file = Path.GetFullPath(file);
-            }
-            catch (Exception e) { RaiseAndExit(e, 1); }
+            app.OnExecute(async () =>
+                    {
+                        // Get file name
+                        var fileArg = app.Arguments.Find((arg) => arg.Name == "file");
+                        var file = fileArg.Value;
+                        if (string.IsNullOrEmpty(file))
+                        {
+                            file = "./Program.cs";
+                        }
 
-            if (!File.Exists(file)) { RaiseAndExit($"File '{file}' not found", 1); }
+                        // Get file path
+                        try
+                        {
+                            file = Path.GetFullPath(file);
+                        }
+                        catch (Exception e) { RaiseAndExit(e, 1); }
 
-            // Execute and show result
-            try
-            {
-                var code = File.ReadAllText(file);
-                await CSharpScript.EvaluateAsync(code);
-            }
-            catch (Exception e) { RaiseAndExit(e, 1); }
+                        if (!File.Exists(file))
+                        {
+                            RaiseAndExit($"File '{file}' not found", 1);
+                            return 1;
+                        }
+
+                        // Execute and show result
+                        try
+                        {
+                            var code = File.ReadAllText(file);
+                            await CSharpScript.EvaluateAsync(code);
+                            return 0;
+                        }
+                        catch (Exception e)
+                        {
+                            RaiseAndExit(e, 1);
+                            return 1;
+                        }
+                    });
+
+            app.Execute(args);
         }
 
         private static void RaiseAndExit(string e, int exitCode)
@@ -51,5 +68,8 @@ namespace QuickSharp
         }
     }
 }
+
+
+
 
 
